b12e84e9da206140c0812378cf675b985730fdbf
Alexander Nilsen <alexander.nilsen.no@gmail.com>
2016-03-06 09:42:39 +0100

    Add initial Flickr Slideshow code and styles
    * Default size 480x270 (1/4th of 1080p)
    * Initial support for "TAGS" and "NUM_ITEMS" URI query parameters (not
      currently used)

---
+++ shared/code.js
@@ -0,0 +1,7 @@
+﻿/**
+  * <http://stackoverflow.com/a/5158301>
+  */
+function getParameterByName(name) {
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
